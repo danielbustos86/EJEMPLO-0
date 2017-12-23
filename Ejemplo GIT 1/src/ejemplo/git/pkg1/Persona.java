9924855223ecaa6440c957fd22b418aa1238d7b4
danielbustos86 <danielbustos86@gmail.com>
2017-12-23 00:29:31 -0300

    Primera version

---
+++ Ejemplo GIT 1/src/ejemplo/git/pkg1/Persona.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo.git.pkg1;
+
+/**
+ *
+ * @author daniel
+ */
+public class Persona {
+    private String rut;
+    private String nombre;
+    
+}
